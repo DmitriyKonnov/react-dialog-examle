be11b22a4a8c1efb580e42c606f6ef2052fcfea8
Dmitriy <d.konnov@gmail.com>
2022-05-05 14:20:21 +0300

    Добавил шапку

---
+++ src/components/Header/UserInfo/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.css';
+
+const UserInfo = () => {
+    return (
+        <div className='user-info'>
+        <div className='name'>Ilez Dev</div>
+        <div className='status'>
+            <span className='circle'/>В сети
+        </div>
+    </div>
+    )
+}
+
+export default UserInfo;
